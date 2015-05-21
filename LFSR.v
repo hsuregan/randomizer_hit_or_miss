6eaf15cf0dbb955a19378e45e3833ee0ff4131e1
hsuregan <hsuregan@gmail.com>
2015-05-20 17:56:43 -0700

    LSFR refactor

--- LFSR.v
+++ LFSR.v
@@ -2,11 +2,11 @@ module LFSR (
 	//input enable,
     input clock,
     input reset,
-    output [12:0] rnd
+    output [12:0] rnd,
+    output [7:0] LED,
     );
- 
 
- 
+reg [3:0] final;
 reg [12:0] random, random_next, random_done;
 reg [3:0] count, count_next; //to keep track of the shifts
 wire feedback = random[12] ^ random[3] ^ random[2] ^ random[0];
@@ -30,7 +30,7 @@ always @ (*)
 begin
  random_next = random; //default state stays the same
  count_next = count;
-   
+ 
   random_next = {random[11:0], feedback}; //shift left the xor'd every posedge clock
   count_next = count + 1;
  
@@ -41,8 +41,22 @@ begin
  end
   
 end
- 
- 
+
+always @(posedge clock posedge reset)
+begin
+	final = random_done[3:0];
+end
+
+assign LED =(final == 0) ? 8'b00000001 :
+			(final == 1) ? 8'b00000010 :
+			(final == 2) ? 8'b00000100 :
+			(final == 3) ? 8'b00001000 :
+			(final == 4) ? 8'b00010000 :
+			(final == 5) ? 8'b00100000 :
+			(final == 6) ? 8'b01000000 :
+			(final == 7) ? 8'b10000000 : 8'b00000001; 
+
+
 assign rnd = random_done;//enable ? random_done : 0;
  
 endmodule
